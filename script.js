2bce7efbf185ad03b5fb63f83a369e38de2a58ac
Artyom Torbokov <mail4242@bk.ru>
2024-12-12 22:22:48 +0700

    fix

--- script.js
+++ script.js
@@ -37,13 +37,14 @@ function chatWidget(){
     }
   
     this.setupFonts = () => {
-      const head = document.querySelector('head')
-      const font = document.createElement('div')
-      font.innerHTML = `<link rel="preconnect" href="https://fonts.googleapis.com">
-                        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-                        <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">`
-      head.appendChild(font.firstElementChild)
-    }
+      const head = document.querySelector('head');
+      const font = document.createElement('div');
+      font.innerHTML = `
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;700&display=swap&v=${Date.now()}" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      `;
+      head.appendChild(font.firstElementChild);
+    };
   
     this.insertChat = (colorSettings)=>{
       document.head.insertAdjacentHTML('beforeend', '<link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500&display=swap" rel="stylesheet">');  
